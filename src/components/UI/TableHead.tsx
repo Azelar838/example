6d3917c0c0fbebae28ecd2614ebcf78fa7d38173
wissem chaaben <chaabenwissem838@gmail.com>
2024-04-06 14:23:09 +0100

    commit3

--- src/components/UI/TableHead.tsx
+++ src/components/UI/TableHead.tsx
@@ -8,12 +8,7 @@ const TableHead = () => {
   if (currentLocation === '/starred') tableDate = 'Date Starred';
   if (currentLocation === '/archived') tableDate = 'Date Archived';
   return (
-    <ul className="table-head">
-      <li className="name">Name</li>
-      {<li className="date">{tableDate}</li>}
-      <li className={currentLocation === '/' ? 'size-home' : ''}>Size</li>
-      {currentLocation !== '/' && <li className="actions">Actions</li>}
-    </ul>
+  <h1>LLLLLLLLLLLL</h1>
   );
 };
 
